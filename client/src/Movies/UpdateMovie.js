5cea305ab23d28f6d141a0cbcc1e3f276057f07a
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2020-01-30 17:13:23 -0300

    UpdateForm inputs working, still need to do put call

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const initialMovie = {
     id: '',
@@ -10,15 +11,33 @@ const initialMovie = {
   };
   
 const UpdateMovie = () => {
-    const [movie, setMovie] = useState(initialMovie);
+    const [movies, setMovies] = useState([]);
+    const [specificMovie, setSpecificMovie] = useState(initialMovie);
     const { id } = useParams();
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/movies')
+        .then(res => setMovies(res.data))
+        .catch(err => console.log(err))
+    }, [])
+
+    // console.log(movies);
+
+    useEffect(() => {
+        console.log(movies);
+        const movieToUpdate = movies.find(m => `${m.id}` === id);
+        
+        if (movieToUpdate) {
+            setSpecificMovie(movieToUpdate);
+        }
+    }, [movies, id])
 
     const handleChanges = e => {
         e.persist();
         let value = e.target.value;
 
-        setMovie({
-            ...movie,
+        setSpecificMovie({
+            ...specificMovie,
             [e.target.name]: value
         })
     };
@@ -26,9 +45,9 @@ const UpdateMovie = () => {
     return (
         <div>
             <form>
-                <input type="text" name="title" className="input" />
-                <input type="text" name="director" className="input" />
-                <input type="text" name="metascore" className="input" />
+                <input type="text" name="title" className="input" value={specificMovie.title} onChange={handleChanges} />
+                <input type="text" name="director" className="input" value={specificMovie.director} onChange={handleChanges} />
+                <input type="text" name="metascore" className="input" value={specificMovie.metascore} onChange={handleChanges} />
                 <button>Submit</button>
             </form>
         </div>
@@ -36,4 +55,5 @@ const UpdateMovie = () => {
 };
 
 export default UpdateMovie;
+
 
